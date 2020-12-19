072b32a4b4597c9e380923416c85501abaf054ed
EC2 Default User <ec2-user@ip-172-31-46-116.ap-northeast-1.compute.internal>
2020-12-19 09:06:11 +0000

    [add]devise-login-divide

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    
+    protected
+    def configure_permitted_parameters
+        add_attrs = [:email, :name, :password, :password_confirmation]
+        devise_parameter_sanitizer.permit :sign_in, keys: add_attrs
+    end
 end
